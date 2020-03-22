6a7b5c328e120ae3e028fe88b3e2dadc1f369cb1
Vahid Montazeri <vmontazery@gmail.com>
2020-03-22 15:44:27 -0700

    first version

---
+++ setup_experiment.m
@@ -0,0 +1,8 @@
+clear
+close all
+clc;
+
+response = {'listener', 'factor1_level', 'factor2_level', 'F3_start', 'F3_end', 'response'};
+save('resnpose.mat', 'response');
+
+disp('Setup completed!');
