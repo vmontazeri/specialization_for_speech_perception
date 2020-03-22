219c095a6695acff00f09cc09d1fdb9cbca9c49e
Vahid Montazeri <vmontazery@gmail.com>
2020-03-22 16:12:35 -0700

    fixed a bug in setup_exp

--- setup_experiment.m
+++ setup_experiment.m
@@ -3,6 +3,6 @@ close all
 clc;
 
 response = {'listener', 'factor1_level', 'factor2_level', 'F3_start', 'F3_end', 'response'};
-save('resnpose.mat', 'response');
+save('response.mat', 'response');
 
 disp('Setup completed!');
