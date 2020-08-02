10519cc54740a15f0957020b7fd86b6939bc32b0
Vahid Montazeri <vmontazery@gmail.com>
2020-08-01 22:16:59 -0700

    exps1&2, recovery mode

--- setup_experiment.m
+++ setup_experiment.m
@@ -3,7 +3,10 @@ close all
 clc;
 
 response = {'listener', 'factor1_level', 'factor2_level', 'F3_start', 'F3_end', 'response'};
-save('response.mat', 'response');
+save('response_experiment1.mat', 'response');
+
+response = {'listener', 'factor1_level', 'factor2_level', 'F3_f_start_1', 'F3_f_start_2', 'F3_f_end', 'chirp_ear', 'speech_ear', 'response'};
+save('response_experiment2.mat', 'response');
 
 training_response = {'listener', 'factor1_level', 'factor2_level', 'F3_start', 'F3_end', 'response'};
 save('training_response.mat', 'training_response');
